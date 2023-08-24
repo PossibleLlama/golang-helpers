39f0c4298030e7f62927ae9d68b6cae7d3591a67
Chris Collier <possiblellama@gmail.com>
2023-08-24 21:24:29 +0100

    Add tests for output of logging

--- logging/logger_test.go
+++ logging/logger_test.go
@@ -53,3 +53,50 @@ func TestLoggerResponse(t *testing.T) {
 	assert.Equal(t, currentLog.Level, zapcore.InfoLevel)
 	assert.Equal(t, currentLog.Message, "finished request")
 }
+
+func TestLoggerLogs(t *testing.T) {
+	var tests = []struct {
+		name  string
+		f     func()
+		level zapcore.Level
+	}{
+		{
+			name:  "info logging",
+			f:     func() { LogInfo("a", "b") },
+			level: zapcore.InfoLevel,
+		}, {
+			name:  "warn logging",
+			f:     func() { LogWarn("a", "b") },
+			level: zapcore.WarnLevel,
+		}, {
+			name:  "error logging",
+			f:     func() { LogError("a", "b") },
+			level: zapcore.ErrorLevel,
+		},
+	}
+
+	for _, testItem := range tests {
+	observedZapCore, observedLogs := observer.New(zap.InfoLevel)
+	observedLogger := zap.New(observedZapCore)
+
+	// Set defaults via init
+	InitLogger("v0.1.2", "proj", "svc")
+	// Replace actual logger with dummy
+	globalLogger = observedLogger
+
+		t.Run(testItem.name, func(t *testing.T) {
+	assert.Empty(t, observedLogs.Len(), "Before logging, there should be no lines")
+
+			testItem.f()
+
+	assert.NotEmpty(t, observedLogs.Len(), "At least one log should have been output")
+	assert.Len(t, observedLogs.All(), 1, "There should be exactly one log per ran test")
+	currentLog := observedLogs.All()[0]
+
+	assert.NotEmpty(t, currentLog.Message, "Output from logger should not be empty")
+
+			assert.Equal(t, currentLog.Level, testItem.level, "Output from logger should be at the correct level")
+	assert.Equal(t, currentLog.Message, "b", "Output from logger should contain line")
+		})
+	}
+}
